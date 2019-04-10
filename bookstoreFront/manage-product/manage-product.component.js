47840f2ead9e792a479cc6c4299a850b9e39fc63
SunHao <2457302636@qq.com>
2019-04-10 20:49:23 +0800

    完成修改商品前后端连接

--- bookstoreFront/manage-product/manage-product.component.js
+++ bookstoreFront/manage-product/manage-product.component.js
@@ -6,6 +6,84 @@ component('manageProduct', {
 		var uri1 = 'http://localhost:8080';
 		var uri = uri1;
 
-		
+		$scope.getProductListModelMessage = '';
+		$scope.getProductListModel = function() {
+			var productKind = $location.search()['productKind'];
+			$http({
+					withCredentials: true,
+					method: 'GET',
+					url: uri + '/manageProduct/getManageProductViewModel',
+					params: ({}),
+				}).then(function success(response) {
+					var data = response.data;
+					if (data.success) {
+						var product = data.productList;
+						$scope.productKindList = data.productKindList;
+						$scope.productList = data.productList;
+						//console.log($scope.productList);
+					} else {
+						$scope.getProductListModelMessage = 'not success';
+					}
+				}),
+				function error(response) {
+					$scope.getProductListModelMessage = 'network error';
+				}
+		}
+		$scope.getProductListModel();
+
+		$scope.updateProductByIdMessage = '';
+		$scope.updateProductById = function(productId, productName, productKind, productPrice, productCount, productDesc) {
+			//console.log(productId, productName, productKind, productPrice, productCount, productDesc)
+			$http({
+					withCredentials: true,
+					method: 'PUT',
+					url: uri + '/manageProduct/updateProductById',
+					params: ({
+						productId: productId,
+						productName: productName,
+						productKind: productCount,
+						productPrice: productPrice,
+						productCount: productCount,
+						productDesc: productDesc
+					}),
+				}).then(function success(response) {
+					var data = response.data;
+					if (data.success) {
+						alert('success');
+						$scope.getProductListModel();
+					} else {
+						$scope.updateProductByIdMessage = 'not success';
+					}
+				}),
+				function error(response) {
+					alert('error');
+					$scope.updateProductByIdMessage = 'network error';
+				}
+		}
+
+		$scope.deleteProductByIdMessage = '';
+		$scope.deleteProductById = function(productId) {
+			//console.log(productId, productName, productKind, productPrice, productCount, productDesc)
+			$http({
+					withCredentials: true,
+					method: 'PUT',
+					url: uri + '/manageProduct/deleteProductById',
+					params: ({
+						productId: productId
+					}),
+				}).then(function success(response) {
+					var data = response.data;
+					if (data.success) {
+						alert('success');
+						$scope.getProductListModel();
+					} else {
+						$scope.deleteProductByIdMessage = 'not success';
+					}
+				}),
+				function error(response) {
+					$scope.deleteProductByIdMessage = 'network error';
+				}
+		}
+
 	}]
 })
